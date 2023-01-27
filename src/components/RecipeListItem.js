df0e43a8b2456aebc7176c96d27fdae355fb6754
juliachernik <116951695+juliachernik@users.noreply.github.com>
2023-01-27 21:57:06 +0100

    Protect Create and Edit pages only for authorized users

--- src/components/RecipeListItem.js
+++ src/components/RecipeListItem.js
@@ -2,20 +2,12 @@ import { useNavigate } from 'react-router-dom';
 
 export default function RecipeListItem({ data }) {
     const navigate = useNavigate();
-    let { title,  imageUrl,  description, author, totalTime, servings, ingredients, _id } = data;
+    let { title } = data;
     return (
         <div className="card" onClick={() => navigate(`/${_id}`)} >
-            <img src={imageUrl} alt={title} width='100px'/>
             <div>
                 <div>{title}</div>
-                <div>{description}</div>
-                <div>{author}</div>
-                <div>{totalTime}</div>
-                <div>{servings}</div>
-                <div>{ingredients}</div>
             </div>
-
         </div>
     )
-    
 }
